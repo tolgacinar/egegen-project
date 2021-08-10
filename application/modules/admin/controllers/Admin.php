f761c43dda7ace1c1c30fa98e9401655d0504710
tolgacinar <tolgan23@gmail.com>
2021-08-10 22:16:14 +0300

    HMVC testleri

---
+++ application/modules/admin/controllers/Admin.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends MX_Controller {
+
+	public function index() {
+		$this->layout->render("main");
+	}
+
+}
+
+/* End of file Admin.php */
+/* Location: ./application/modules/admin/controllers/Admin.php */
