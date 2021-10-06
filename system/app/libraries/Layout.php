6306bf270d17a7a16114093e77004a7112d5c8de
tolgacinar <tolgan23@gmail.com>
2021-10-06 20:13:36 +0300

    Modül güncellemesi
    - Modüllerin yolları değiştirildi
    - Yeni structure göre htaccess yapılandırıldı.
    - AdminLTE teması entegre edildi.

---
+++ system/app/libraries/Layout.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Layout {
+	protected $ci;
+	public $data = [];
+
+	public function __construct() {
+        $this->ci =& get_instance();
+        $this->title("");
+		$this->ci->load->model('site/common_model');
+        $this->data['menus'] = $this->ci->common_model->getMenu();
+	}
+
+	public function render($view, $data = []) {
+		$data = array_merge($this->data, $data);
+		$this->ci->load->view("includes/header", $data);
+		$this->ci->load->view($view);
+		$this->ci->load->view("includes/footer", $data);
+	}
+
+	public function title($title) {
+		$this->data['page_title'] = $title . " - Site Adı";
+	}
+
+}
+
+/* End of file Layout.php */
+/* Location: ./application/libraries/Layout.php */
